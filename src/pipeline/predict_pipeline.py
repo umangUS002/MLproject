24d27fd2bd40f392dace59daca97e4069b73880f
umangUS002 <btech10271.23@bitmesra.ac.in>
2025-07-24 17:14:05 +0530

    added python.config

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,26 +2,32 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
-
+import os
 
 class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self,features):
+    def predict(self, features):
         try:
-            model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
             print("Before Loading")
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
             print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
+
+            print("Input Columns:", features.columns.tolist())
+            print("Preprocessor expects:", preprocessor.feature_names_in_)
+
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
             return preds
-        
+
         except Exception as e:
-            raise CustomException(e,sys)
+            print("❌ Exception in PredictPipeline:", e)
+            raise CustomException(e, sys)
+
 
 
 
